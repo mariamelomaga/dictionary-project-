a4aee109da06e963991eef8042b616cdfe43780c
mariamelomaga <maria.melo.maga@gmail.com>
2021-09-08 22:29:38 +0100

    Search engine setup  - dictionary component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.png';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> 
-        di ctio o na ry 
+        <img src={logo} className="App-logo" alt="logo" />
+        di ctio o na ry
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"> coded by Maria Melo</footer>
     </div>
   );
 }
-
-
