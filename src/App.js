8119a4e2d77ca16b1e317bb7a9379f39d67f6710
mariamelomaga <maria.melo.maga@gmail.com>
2021-09-18 18:31:31 +0100

    Phonetic component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        di ctio o na ry
+        di ctio na ry
       </header>
       <main>
         <Dictionary />
