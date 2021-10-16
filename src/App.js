01c37ca9e92b60803b273b7c7f58cc9e88168a82
mariamelomaga <maria.melo.maga@gmail.com>
2021-10-16 16:12:18 +0100

    Removed logo + added links for Git and Netlify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -6,13 +5,25 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />{" "}
         <h1>D i ctio na ry</h1>
       </header>
       <main>
-        <Dictionary defaultKeyword="sun" />
+        <Dictionary defaultKeyword="plants" />
       </main>
-      <footer className="App-footer"> coded by Maria Melo</footer>
+      <footer className="App-footer">
+        {" "}
+        coded by Maria Melo | open-sourced{" "}
+        <a
+          href="https://github.com/mariamelomaga/dictionary-project-"
+          target="_blank"
+        >
+          {" "}
+          on GitHub
+        </a>{" "}
+        {}
+        and hosted on{" "}
+        <a href="https://priceless-tesla-8f038b.netlify.app/">Netlify</a>
+      </footer>
     </div>
   );
 }
